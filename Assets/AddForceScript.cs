221211539483521aec4ea5be507e7e9b52eabf96
49380211 <49380211@est.ort.edu.ar>
2025-09-02 10:42:06 -0300

    x

---
+++ Assets/AddForceScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceScript : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            PushUp();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            RotateRB();
+        }
+    }
+
+    void PushUp()
+    {
+        rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+    }
+
+    void RotateRB()
+    {
+        rb.AddTorque(Vector3.up * force, ForceMode.Impulse);
+    }
+}
---
+++ Assets/CannonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed;
+    [SerializeField] Transform firePointTR;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootingForce;
+
+    
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.right * x * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bulletClone = Instantiate(bullet, firePointTR.position, transform.rotation);
+            bulletClone.GetComponent<Rigidbody>().AddForce(Vector3.up * shootingForce, ForceMode.Impulse);
+        }
+    }
+}
